9a71b9fbd9aaaddcb415c3c469f0ad8dca41ed30
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-28 10:40:27 +0200

    Проект готов

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Form from "./components/form";
+import Check from "./components/check";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : {
+        text: '',
+        cost: '',
+        items: [
+            {title: 'Games', price: 230, type: 'entertainment', id: 1},
+            {title: 'Potato', price: 100, type: 'food', id: 2},
+            {title: 'wheels', price: 999, type: 'car', id: 3},
+        ]
+    };
+    counter = this.state.items.length > 0 ? this.state.items[this.state.items.length - 1].id : 0;
+
+    render() {
+        return (
+            <div className="appBlock">
+                <Form
+                    changeText={this.changeText}
+                    changeCost={this.changeCost}
+                    onClick={this.addItem}
+                />
+                <Check
+                    state={this.state.items}
+                    onClick={this.removeItem}
+                />
+            </div>
+        );
+    }
+
+    changeText = event => {
+        this.setState({text: event.target.value});
+    };
+    changeCost = event => {
+        this.setState({cost: event.target.value});
+    };
+    addItem = event => {
+        event.preventDefault();
+        if (this.state.text.length > 0 && this.state.cost.length > 0 && !isNaN(this.state.cost)) {
+            const items = [...this.state.items];
+            items.push({ title: this.state.text, price: parseInt(this.state.cost), type: document.querySelector('.inputSelect').value, id: ++this.counter });
+
+            localStorage.setItem('items', JSON.stringify({items: items, text: '', cost: ''}));
+            this.setState({items: items, text: '', cost: ''});
+        }
+        this.setState({text: '', cost: ''});
+        document.querySelector('.textInput').value = '';
+        document.querySelector('.costInput').value = '';
+    };
+    removeItem = id => {
+        const items = [...this.state.items];
+        const index = items.findIndex(item => item.id === id);
+        items.splice(index, 1);
+
+        localStorage.setItem('items', JSON.stringify({items: items, text: '', cost: ''}));
+        this.setState({items: items});
+    }
 }
 
 export default App;
